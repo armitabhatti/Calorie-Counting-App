cdf6dd63cbfaf79e5cb6fa328895dd7c3dbdabc5
armita <armita.bhatti@gmail.com>
2024-04-01 13:06:55 -0400

    Initial Commit

---
+++ MyCCApp/MyCCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCCAppApp.swift
+//  MyCCApp
+//
+//  Created by armita on 2024-04-01.
+//
+
+import SwiftUI
+
+@main
+struct MyCCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
